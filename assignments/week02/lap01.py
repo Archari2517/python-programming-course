c1d6a07fe0cf68ad2d872476eb1ea96b29370e81
Archari khemthong <Archari.k@ku.th>
2025-07-15 21:23:10 +0700

    change wrong word

--- assignments/week02/lap01.py
+++ assignments/week02/lap01.py
@@ -27,7 +27,7 @@ if bmi < 18.5 :
     print("Underweight")
 
 if bmi >= 18.5 and bmi <= 24.9 :
-    print("Underweight")
+    print("Normal weightห")
 
 if bmi >= 25.0 and bmi <= 29.9 :
     print("Overweight")
